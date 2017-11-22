a7af11455dbdf5b2cd2983017d34283397fec37f
Sarah Alexandra Maria Van Dam <s153659@student.dtu.dk>
2017-11-22 13:40:01 +0100

    txt file

---
+++ cmake-build-debug/CMakeFiles/ComputersystemerA3.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/src/alarm.c" "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/cmake-build-debug/CMakeFiles/ComputersystemerA3.dir/src/alarm.c.o"
+  "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/src/radio.c" "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/cmake-build-debug/CMakeFiles/ComputersystemerA3.dir/src/radio.c.o"
+  "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/src/radio_test.c" "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/cmake-build-debug/CMakeFiles/ComputersystemerA3.dir/src/radio_test.c.o"
+  "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/src/simsec.c" "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/cmake-build-debug/CMakeFiles/ComputersystemerA3.dir/src/simsec.c.o"
+  "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/src/simsec_test.c" "/Users/sarahdam/Dropbox/Universitet/5. semester/02132 Computersystemer/Assignment 3/ComputersystemerA3/cmake-build-debug/CMakeFiles/ComputersystemerA3.dir/src/simsec_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
